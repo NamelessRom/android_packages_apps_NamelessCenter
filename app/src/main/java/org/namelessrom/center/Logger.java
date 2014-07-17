6fdea8c7707816dba9f0c1f887432093a08870cf
Evisceration <eviscerationls@gmail.com>
2014-07-17 12:40:23 +0200

    Initial menu implementation
    Change-Id: I174b852fe5f4dade37e6cd9d1a773d8f35c55077

---
+++ app/src/main/java/org/namelessrom/center/Logger.java
@@ -0,0 +1,55 @@
+/*
+ * <!--
+ *    Copyright (C) 2014 Alexander "Evisceration" Martinz
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * -->
+ */
+
+package org.namelessrom.center;
+
+import android.util.Log;
+
+/**
+ * A Logging utility
+ */
+public class Logger {
+
+    private static final boolean DEBUG = false;
+
+    public static void d(final Object object, final String msg) {
+        if (DEBUG) Log.d(object.getClass().getSimpleName(), "--> " + msg);
+    }
+
+    public static void e(final Object object, final String msg) {
+        if (DEBUG) Log.e(object.getClass().getSimpleName(), "--> " + msg);
+    }
+
+    public static void i(final Object object, final String msg) {
+        if (DEBUG) Log.i(object.getClass().getSimpleName(), "--> " + msg);
+    }
+
+    public static void v(final Object object, final String msg) {
+        if (DEBUG) Log.v(object.getClass().getSimpleName(), "--> " + msg);
+    }
+
+    public static void w(final Object object, final String msg) {
+        if (DEBUG) Log.w(object.getClass().getSimpleName(), "--> " + msg);
+    }
+
+    public static void wtf(final Object object, final String msg) {
+        if (DEBUG) Log.wtf(object.getClass().getSimpleName(), "--> " + msg);
+    }
+
+}
---
+++ app/src/main/java/org/namelessrom/center/utils/AnimationHelper.java
@@ -0,0 +1,74 @@
+/*
+ * <!--
+ *    Copyright (C) 2014 Alexander "Evisceration" Martinz
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * -->
+ */
+
+package org.namelessrom.center.utils;
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+
+import org.namelessrom.center.AppInstance;
+
+/**
+ * Helper class for animations
+ */
+public class AnimationHelper {
+
+    private static final int DEFAULT_DURATION = 250;
+
+    public static ObjectAnimator scaleX(final View v, final float from, final float to,
+            final int duration) {
+        final ObjectAnimator animator = ObjectAnimator.ofFloat(v, "scaleX", from, to);
+        animator.setDuration(duration);
+        return animator;
+    }
+
+    public static AnimatorSet buildScaleUpAnimation(final View target, final float targetScaleX,
+            final float targetScaleY) {
+        final AnimatorSet scaleUp = new AnimatorSet();
+        scaleUp.playTogether(
+                ObjectAnimator.ofFloat(target, "scaleX", targetScaleX),
+                ObjectAnimator.ofFloat(target, "scaleY", targetScaleY)
+        );
+        scaleUp.setDuration(DEFAULT_DURATION);
+        return scaleUp;
+    }
+
+    public static AnimatorSet buildAlphaAnimation(final View target, final float alpha) {
+        final AnimatorSet alphaAnimation = new AnimatorSet();
+        alphaAnimation.playTogether(ObjectAnimator.ofFloat(target, "alpha", alpha));
+        alphaAnimation.setDuration(DEFAULT_DURATION);
+        return alphaAnimation;
+    }
+
+    public static AnimatorSet buildScaleDownAnimation(final View target, final float targetScaleX,
+            final float targetScaleY) {
+        final AnimatorSet scaleDown = new AnimatorSet();
+        scaleDown.playTogether(
+                ObjectAnimator.ofFloat(target, "scaleX", targetScaleX),
+                ObjectAnimator.ofFloat(target, "scaleY", targetScaleY)
+        );
+        scaleDown.setInterpolator(AnimationUtils.loadInterpolator(AppInstance.applicationContext,
+                android.R.anim.decelerate_interpolator));
+        scaleDown.setDuration(DEFAULT_DURATION);
+        return scaleDown;
+    }
+
+}
