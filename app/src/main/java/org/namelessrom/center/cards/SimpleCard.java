dbb2a16baa68ece7ad5927b0eec1b7d4a0084ee4
Evisceration <eviscerationls@gmail.com>
2014-07-21 08:29:05 +0200

    make the simple card a bit more useful
    Change-Id: Ifdb3e907f841954b47ad09cf3e2a74a0e8db363e

--- app/src/main/java/org/namelessrom/center/cards/SimpleCard.java
+++ app/src/main/java/org/namelessrom/center/cards/SimpleCard.java
@@ -6,7 +6,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import org.namelessrom.center.AppInstance;
+import org.namelessrom.center.Constants;
 import org.namelessrom.center.R;
+import org.namelessrom.center.items.UpdateInfo;
+import org.namelessrom.center.utils.Helper;
 
 import it.gmariotti.cardslib.library.internal.Card;
 
@@ -37,12 +41,13 @@ public class SimpleCard extends Card {
         final TextView title = findById(view, R.id.simple_message_card_title);
         final TextView body = findById(view, R.id.simple_message_card_text);
 
-        if (!TextUtils.isEmpty(getTitle())) {
-            title.setText(getTitle());
-        }
-        if (!TextUtils.isEmpty(getBody())) {
-            body.setText(getBody());
-        }
+        title.setText(String.format("%s: %s", AppInstance.getStr(R.string.installed_version),
+                UpdateInfo.getReadableName(
+                        "nameless-" + Helper.readBuildProp("ro.nameless.version"))
+        ));
+        body.setText(String.format("%s\n%s: %s", AppInstance.getStr(R.string.swipe_refresh),
+                AppInstance.getStr(R.string.size_used),
+                Helper.humanReadableByteCount(Helper.dirSize(Constants.UPDATE_FOLDER_FULL))));
     }
 
 }
