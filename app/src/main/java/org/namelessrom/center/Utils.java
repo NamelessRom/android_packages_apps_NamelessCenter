4103ebbce4fae073bee51c169732eeb04cc18fdc
Evisceration <eviscerationls@gmail.com>
2014-10-03 16:16:33 +0200

    move to Paranoid OTA as base
    Change-Id: I3c4985dda5fec8157cc7513e51228db0544b5d5a

---
+++ app/src/main/java/org/namelessrom/center/Utils.java
@@ -0,0 +1,282 @@
+/*
+ * Copyright 2014 ParanoidAndroid Project
+ *
+ * This file is part of Paranoid OTA.
+ *
+ * Paranoid OTA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Paranoid OTA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Paranoid OTA.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.namelessrom.center;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.BitmapFactory;
+import android.graphics.Typeface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v4.app.NotificationCompat;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.namelessrom.center.helpers.SettingsHelper;
+import org.namelessrom.center.updater.Updater;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.lang.reflect.Method;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Utils {
+
+    public static final String FILES_INFO               = "org.namelessrom.center.Utils.FILES_INFO";
+    public static final String CHECK_DOWNLOADS_FINISHED =
+            "org.namelessrom.center.Utils.CHECK_DOWNLOADS_FINISHED";
+    public static final String CHECK_DOWNLOADS_ID       =
+            "org.namelessrom.center.Utils.CHECK_DOWNLOADS_ID";
+    public static final String BUILD_DATE               = "ro.nameless.date";
+    public static final int    ROM_ALARM_ID             = 122303221;
+
+    public static final int TWRP      = 1;
+    public static final int CWM_BASED = 2;
+
+    public static Version[] sPackageInfosRom = new Version[0];
+    private static Typeface sRobotoThin;
+
+    public static class NotificationInfo implements Serializable {
+        public int       mNotificationId;
+        public Version[] mPackageInfosRom;
+    }
+
+    public static String getProp(String prop) {
+        try {
+            Process process = Runtime.getRuntime().exec("getprop " + prop);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
+                    process.getInputStream()));
+            StringBuilder log = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                log.append(line);
+            }
+            return log.toString();
+        } catch (IOException e) {
+            // Runtime error
+        }
+        return null;
+    }
+
+    /**
+     * Method borrowed from OpenDelta. Using reflection voodoo instead calling
+     * the hidden class directly, to dev/test outside of AOSP tree.
+     *
+     * @author Jorrit "Chainfire" Jongma
+     * @author The OmniROM Project
+     */
+    public static boolean setPermissions(String path, int mode, int uid, int gid) {
+        try {
+            Class<?> FileUtils = Utils.class.getClassLoader().loadClass("android.os.FileUtils");
+            Method setPermissions = FileUtils.getDeclaredMethod("setPermissions", new Class[]{
+                    String.class,
+                    int.class,
+                    int.class,
+                    int.class
+            });
+            return ((Integer) setPermissions.invoke(null, path, mode, uid, gid) == 0);
+        } catch (Exception e) {
+            // A lot of voodoo could go wrong here, return failure instead of crash
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static String getDateAndTime() {
+        return new SimpleDateFormat("yyyy-MM-dd.HH.mm.ss").format(new Date(System
+                .currentTimeMillis()));
+    }
+
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    public static void setAlarm(Context context, boolean trigger) {
+
+        SettingsHelper helper = new SettingsHelper(context);
+        setAlarm(context, helper.getCheckTime(), trigger);
+    }
+
+    public static void setAlarm(Context context, long time, boolean trigger) {
+
+        Intent i = new Intent(context, NotificationAlarm.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        PendingIntent pi = PendingIntent.getBroadcast(context,
+                ROM_ALARM_ID, i,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        am.cancel(pi);
+        if (time > 0) {
+            am.setInexactRepeating(AlarmManager.RTC_WAKEUP, trigger ? 0 : time, time, pi);
+        }
+    }
+
+    public static boolean alarmExists(Context context) {
+        return (PendingIntent.getBroadcast(context, ROM_ALARM_ID,
+                new Intent(context, NotificationAlarm.class),
+                PendingIntent.FLAG_NO_CREATE) != null);
+    }
+
+    public static void showToastOnUiThread(final Context context, final int resourceId) {
+        ((Activity) context).runOnUiThread(new Runnable() {
+
+            public void run() {
+                Toast.makeText(context, resourceId, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public static void showToastOnUiThread(final Context context, final String string) {
+        ((Activity) context).runOnUiThread(new Runnable() {
+
+            public void run() {
+                Toast.makeText(context, string, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public static void showNotification(Context context, Version[] infosRom) {
+        Resources resources = context.getResources();
+
+        if (infosRom != null) {
+            sPackageInfosRom = infosRom;
+        } else {
+            infosRom = sPackageInfosRom;
+        }
+
+        Intent intent = new Intent(context, MainActivity.class);
+        NotificationInfo fileInfo = new NotificationInfo();
+        fileInfo.mNotificationId = Updater.NOTIFICATION_ID;
+        fileInfo.mPackageInfosRom = infosRom;
+        intent.putExtra(FILES_INFO, fileInfo);
+        PendingIntent pIntent = PendingIntent.getActivity(context, Updater.NOTIFICATION_ID, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setContentTitle(resources.getString(R.string.new_system_update))
+                .setSmallIcon(R.drawable.ic_launcher_mono)
+                .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_launcher))
+                .setContentIntent(pIntent);
+
+        final String contextText;
+        if (infosRom.length == 1) {
+            final String filename = infosRom[0].getReadableName();
+            contextText = resources.getString(R.string.new_package_name, filename);
+        } else {
+            contextText = resources.getString(R.string.new_packages, infosRom.length);
+        }
+        builder.setContentText(contextText);
+
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+        inboxStyle.setBigContentTitle(context.getResources().getString(R.string.new_system_update));
+        if (infosRom.length > 1) {
+            inboxStyle.addLine(contextText);
+        }
+        for (final Version version : infosRom) {
+            inboxStyle.addLine(version.getReadableName());
+        }
+        inboxStyle.setSummaryText(resources.getString(R.string.app_name));
+        builder.setStyle(inboxStyle);
+
+        Notification notif = builder.build();
+
+        NotificationManager notificationManager = (NotificationManager) context
+                .getSystemService(Service.NOTIFICATION_SERVICE);
+
+        notif.flags |= Notification.FLAG_AUTO_CANCEL;
+
+        notificationManager.notify(Updater.NOTIFICATION_ID, notif);
+    }
+
+    public static String exec(String command) {
+        try {
+            Process p = Runtime.getRuntime().exec(command);
+            DataOutputStream os = new DataOutputStream(p.getOutputStream());
+            os.writeBytes("sync\n");
+            os.writeBytes("exit\n");
+            os.flush();
+            p.waitFor();
+            return getStreamLines(p.getInputStream());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    private static String getStreamLines(final InputStream is) {
+        String out = null;
+        StringBuffer buffer = null;
+        final DataInputStream dis = new DataInputStream(is);
+
+        try {
+            if (dis.available() > 0) {
+                buffer = new StringBuffer(dis.readLine());
+                while (dis.available() > 0) {
+                    buffer.append("\n").append(dis.readLine());
+                }
+            }
+            dis.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        if (buffer != null) {
+            out = buffer.toString();
+        }
+        return out;
+    }
+
+    public static void setRobotoThin(Context context, View view) {
+        if (sRobotoThin == null) {
+            sRobotoThin = Typeface.createFromAsset(context.getAssets(), "Roboto-Light.ttf");
+        }
+        setFont(view, sRobotoThin);
+    }
+
+    private static void setFont(View view, Typeface robotoTypeFace) {
+        if (view instanceof ViewGroup) {
+            int count = ((ViewGroup) view).getChildCount();
+            for (int i = 0; i < count; i++) {
+                setFont(((ViewGroup) view).getChildAt(i), robotoTypeFace);
+            }
+        } else if (view instanceof TextView) {
+            ((TextView) view).setTypeface(robotoTypeFace);
+        }
+    }
+}
